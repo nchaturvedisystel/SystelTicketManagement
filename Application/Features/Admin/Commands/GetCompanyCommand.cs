0f1c981c991c1cd059ceb29233a9ec180b705394
rchorge <rchorge@systelusa.com>
2023-09-29 10:48:20 +0530

    API Integration completed for getting company list

---
+++ Application/Features/Admin/Commands/GetCompanyCommand.cs
@@ -0,0 +1,30 @@
+﻿using Application.DTOs.Admin;
+using Application.Interfaces.Admin;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Features.Admin.Commands
+{
+    public class GetCompanyCommand : IRequest<CompanyList>
+    {
+    }
+    internal class CompanyHandler : IRequestHandler<GetCompanyCommand, CompanyList>
+    {
+        protected readonly ICompany _Company;
+
+        public CompanyHandler(ICompany Company)
+        {
+            _Company = Company;
+        }
+        public async Task<CompanyList> Handle(GetCompanyCommand request, CancellationToken cancellationToken)
+        {
+            return await _Company.GetCompany();
+        }
+    }
+}
+
