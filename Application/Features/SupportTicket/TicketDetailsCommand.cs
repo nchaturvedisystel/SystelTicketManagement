62538e9b31484de6ae5ee6e30c84a47ded239a64
achaudhari8 <achaudhari@systelusa.com>
2023-10-09 12:08:10 +0530

    TicketDetails API done

---
+++ Application/Features/SupportTicket/TicketDetailsCommand.cs
@@ -0,0 +1,30 @@
+﻿using Application.DTOs.SupportTicket;
+using Application.Interfaces.SupportTicket;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Features.SupportTicket
+{
+    public class TicketDetailsCommand : IRequest<TicketList>
+    {
+        public SupportTicketDTO supportTicketDTO { get; set; }
+    }
+
+    internal class TicketDetailsCommandHandler : IRequestHandler<TicketDetailsCommand, TicketList>
+    {
+        protected readonly ISupportTicket _supportTicket;
+        public TicketDetailsCommandHandler(ISupportTicket supportTicket)
+        {
+            _supportTicket = supportTicket;
+        }
+        public async Task<TicketList> Handle(TicketDetailsCommand request, CancellationToken cancellationToken)
+        {
+            return await _supportTicket.SupportTickets_GetTicketDetails(request.supportTicketDTO);
+        }
+    }
+}
