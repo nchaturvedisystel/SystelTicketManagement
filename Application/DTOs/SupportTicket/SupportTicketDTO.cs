be37b6ffb3bd95c84070f9ffbaab0d88cfb6a6c0
nchaudharisystel <146067336+nchaudharisystel@users.noreply.github.com>
2023-10-19 14:24:41 +0530

    Department & RaisedBy all files chanegs

--- Application/DTOs/SupportTicket/SupportTicketDTO.cs
+++ Application/DTOs/SupportTicket/SupportTicketDTO.cs
@@ -25,8 +25,8 @@ namespace Application.DTOs.SupportTicket
         public string ActualDuration { get; set; }
         public DateTime? TargetDate { get; set; }
         public DateTime ResolutionDate { get; set;}
-        public string AddField1 { get; set; }
-        public string AddField2 { get; set; }
+        public string Department { get; set; }
+        public string RaisedBy { get; set; }
         public string AddField3 { get; set; }
         public string AddField4 { get; set; }
         public string AddField5 { get; set; }
